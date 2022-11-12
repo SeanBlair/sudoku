40a4b4823c49aa42e27711cfa128dd6f57fdb384
Sean Blair <seanblairx@gmail.com>
2022-11-12 15:15:55 -0800

    Remove unneeded html/css from template

---
+++ src/lib/Sudoku.svelte
@@ -0,0 +1,22 @@
+<div class="sudoku-game">
+  <h1>My Sudoku</h1>
+  <div class="board">Board</div>
+  <div class="number-inputs">Number Inputs</div>
+</div>
+
+
+<style>
+  /* .sudoku-game {
+    min-width: 320px; // might want to set some min values for height and width here
+    min-height: 100vh;
+  }
+   */
+  /* .board {
+    
+  }
+
+  .number-inputs {
+
+  } */
+
+</style>
