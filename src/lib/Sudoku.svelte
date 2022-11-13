4338989af42cad8e332ff7550c7058876f470866
Sean Blair <seanblairx@gmail.com>
2022-11-12 18:20:18 -0800

    WIP: create the board with a single aligned number

--- src/lib/Sudoku.svelte
+++ src/lib/Sudoku.svelte
@@ -1,22 +1,57 @@
+<script>
+  let rows = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let columns = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+</script>
+
 <div class="sudoku-game">
   <h1>My Sudoku</h1>
-  <div class="board">Board</div>
-  <div class="number-inputs">Number Inputs</div>
+  <div class="board">
+    {#each rows as row}
+      {#each columns as column}
+        <div class="cell">{`${row}`}</div>
+      {/each}
+    {/each}
+  </div>
+  <div class="number-inputs"></div>
 </div>
 
 
 <style>
-  /* .sudoku-game {
-    min-width: 320px; // might want to set some min values for height and width here
-    min-height: 100vh;
+  .sudoku-game {
+    --width: 400px;
   }
-   */
-  /* .board {
-    
+  
+  .board {
+    display: grid;
+    grid-template-columns: repeat(9, 1fr);
+    /* gap: 1px 1px; */
+    width: fit-content;
+
+    /* width: var(--width);
+    height: 200px; */
+    background-color: aliceblue;
+    color: black;
+    margin: 0 auto;
+  }
+
+  .cell {
+    border: 1px solid;
+    width: 30px;
+    height: 30px;
+    text-align: center;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 
   .number-inputs {
-
-  } */
+    display: grid;
+    width: var(--width);
+    height: 100px;
+    background-color: beige;
+    margin: 0 auto;
+    margin-top: 1rem;
+  }
 
 </style>
