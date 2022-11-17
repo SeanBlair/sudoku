b3cadf6ff175862afb3f06ca62e2647c02c3d0c8
Sean Blair <seanblairx@gmail.com>
2022-11-16 22:16:18 -0800

    POC grid without borders

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,17 @@
 <script>
   import Sudoku from './lib/Sudoku.svelte';
+  import SudokuGrid from './lib/SudokuGrid.svelte';
+  import GridNoBorders from './lib/GridNoBorders.svelte';
+  import GameNoBorders from './lib/GameNoBorders.svelte';
 </script>
 
 <main>
-  <Sudoku />
+  
+  <!-- <Sudoku /> -->
+
+  <!-- <SudokuGrid /> -->
+
+  <!-- <GridNoBorders /> -->
+
+  <GameNoBorders />
 </main>
---
+++ src/lib/SudokuGrid.svelte
@@ -0,0 +1,40 @@
+<script>
+    import { each } from "svelte/internal";
+
+
+  let nine = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+</script>
+
+<h1>My Sudoku Grid</h1>
+<div class="board">
+  {#each nine as group}
+    <div class="group">
+      {#each nine as cell}
+        <div class="cell">{cell}</div>
+      {/each}
+    </div>
+  {/each}
+</div>
+
+<style>
+  .board, .group {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    border: 2px solid; 
+    width: fit-content;   
+  }
+
+  .cell {
+    border: 1px solid;
+    width: 3rem;
+    height: 3rem;
+
+    font-size: 2rem;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+
+</style>
